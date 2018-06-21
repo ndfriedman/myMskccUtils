66b5a1d94edfaf30d7a9b020a001783138e4a876
NoahFriedman <friedman@mskcc.org>
2018-06-21 11:19:12 -0400

    changes to maf subsetter script

--- maf_subsetter.py
+++ maf_subsetter.py
@@ -23,12 +23,43 @@ def main():
 	parser = argparse.ArgumentParser(description='Arg parser for this script')
 	parser.add_argument('--mode', help='mode to run the script in', default='subsetByCancerType')
 	parser.add_argument('--cancerTypes', default='')
-	parser.add_argument('inputMaf', default='')
+	parser.add_argument('--inputMaf', default='/ifs/work/taylorlab/friedman/myUtils/FilteredMafWithHospotAndSignatures.maf')
+	parser.add_argument('--inputMaf2', default='/ifs/work/taylorlab/friedman/myAdjustedDataFiles/impactMafs/data_mutations_extended_mafAnno.maf')
+	parser.add_argument('--outputFilename', default='adjustedMaf.maf')
+	parser.add_argument('--outputDir', default='/ifs/work/taylorlab/friedman/myAdjustedDataFiles/adjustedSubsetMafs')
 	args = parser.parse_args()
+
+	mafDf = pd.read_table(args.inputMaf, skiprows=[0])
+	writePath = os.path.join(args.outputDir, args.outputFilename)
 
 	if args.mode == 'subsetByCancerType':
 		cases = get_cases_for_cancer_type('Bladder_Cancer')
-		print cases
+		mafDf = mafDf[mafDf['Tumor_Sample_Barcode'].isin(cases)]
+		print 'writing file to ', writePath
+		mafDf.to_csv(writePath, sep='\t', index=False)
+
+	if args.mode == 'mashMafs': #mode to merge two mafs
+		mafDf1 = pd.read_table(args.inputMaf)
+		mafDf2 = pd.read_table(args.inputMaf2, skiprows=[0])
+
+		mafDf1['idCol'] = mafDf1.apply(lambda row: str(row['Chromosome']) + '_' + str(row['Start_Position']), axis=1)
+		mafDf2['idCol'] = mafDf2.apply(lambda row: str(row['Chromosome']) + '_' + str(row['Start_Position']), axis=1)
+
+		#print mafDf1['idCol']
+		#print mafDf2['idCol']
+		print len(list(mafDf1['idCol']))
+		print len(list(mafDf2['idCol']))
+		#mergedDf = mafDf1.merge(mafDf2, how='left', left_on='idCol', right_on='idCol')
+		mergedDf = mafDf1.merge(mafDf2, on='idCol')
+
+		#print mergedDf.shape
+		#print mergedDf.columns.values
+		#print mergedDf['idCol']
+		#print mergedDf['Start_Position_x']
+		#print mergedDf['Start_Position_y']
+		print len(set(mafDf1['idCol']) & set(mafDf2['idCol']))
+
+		mergedDf.to_csv('facetsAndTrinucAndHotspotsAndSignatures.maf', index=False, sep='\t')
 
 if __name__ == '__main__':
     main()
