94aab9474f84cda97fab746dd0a91dd8599b369f
NoahFriedman <friedman@mskcc.org>
2019-10-04 12:28:46 -0400

    modest changes

--- signature_attribution_util.py
+++ signature_attribution_util.py
@@ -4,12 +4,12 @@ import argparse
 import os
 import pandas as pd
 import numpy as np
+import imp
 
 from collections import Counter
 sys.path.append('/ifs/work/taylorlab/friedman/')
 
-#from myUtils import mutationSigUtils
-
+analysis_utils = imp.load_source('analysis_utils', '/Users/friedman/Desktop/mnt/ifs/work/taylorlab/friedman/myUtils/analysis_utils.py')
 
 
 #creates the reference four nucleotide context for signatures
@@ -33,16 +33,59 @@ def normalize_probs(counterOfProbs):
 		counterOfProbs[key] /= total
 	return counterOfProbs
 
+
+######NEW FUNCTION 6/13
+#THIS marks cases whose motifs are consistent with the dominant signature pattern in the case (hypermutator motif)
+#and mutations that are not (not hypermutator motif)
+def mark_mutations_in_case_as_motif_outliers(maf):
+
+	#TWO different ways to enumerate related quad nucs: quadnucs above a theshold
+	#quad nucs representing ~Threshold amount of probability mass of quad nucs
+
+	def enumerate_related_quadnucs(normedQuadNucFracs, rawQuadNucCounts):
+		#quadNucsEnrichedForSignature = {x : normedCounter[x] for x in normedCounter if normedCounter[x] >= thresh}
+		
+		RELATED_MAX_FRAC = .75 #THE FRACTION ABOVE WHICH WE WILL START TO CONSIDER QUAD NUCS unrelated
+		MIN_QUAD_NUC_THESH = .03 #the minimum fraction of quad nucs that can be done here
+		MIN_QUAD_NUC_COUNT = 3 #the minimum number of mutations at a quad nuc tgat can occur
+		quadNucFracSum = 0
+
+		relatedQuadnucs = []
+		for quadNuc, val in normedCounter.most_common(len(normedCounter)):
+			quadNucFracSum += val
+			if quadNucFracSum < RELATED_MAX_FRAC and val >= MIN_QUAD_NUC_THESH and rawQuadNucCounts[quadNuc] >= MIN_QUAD_NUC_COUNT:
+				relatedQuadnucs.append(quadNuc)
+			else:
+				return relatedQuadnucs
+	
+		return relatedQuadnucs
+
+	quadNucAetiologyDict = {}
+	for case in set(maf['Tumor_Sample_Barcode']):
+		caseMafWithQuadNucs = maf[(maf['Tumor_Sample_Barcode'] == case) & (maf['quadNuc'].notnull())]
+		quadNucs = caseMafWithQuadNucs['quadNuc']
+		
+		#TRY A NON theshold based method?
+		quadNucCounts = Counter(quadNucs)
+		normedCounter = analysis_utils.normalize_counter(quadNucCounts, nDigitsRound=3)
+		relatedQuadnucs = enumerate_related_quadnucs(normedCounter, quadNucCounts)
+		quadNucAetiologyDict[case] = relatedQuadnucs
+	maf['isRelatedQuadnuc'] = maf.apply(lambda row:
+		None if row['quadNuc'] == None
+		else True if row['quadNuc'] in quadNucAetiologyDict[row['Tumor_Sample_Barcode']]
+		else False, axis=1)
+	return maf
+		
 #given a row delineating signatures in a case goes bit by bit and assigns which signature likely caused a mutation
-def assign_mutation_signature_probabilities(row, spectrumDict, signatureConsiderationMode = 'all', mode='singleAnswer'):
-	quadNuc = create_reference_four_nuc(row['Ref_Tri'], row['Reference_Allele'], row['Tumor_Seq_Allele2'])
+def assign_mutation_signature_probabilities(row, spectrumDict, quadNuc, signatureConsiderationMode = 'all', mode='singleAnswer'):
 	signatures = ['Signature.' + str(i) for i in range(1,31)]
 
-	signatures = signatures[:18] #code to get rid of the "garbage" signatures
-	signatures += ['Signature.20', 'Signature.26', 'Signature.29']
-	signatures.remove('Signature.10')
-	signatures.remove('Signature.11')
-	signatures.remove('Signature.8')
+	#signatures = signatures[:18] #code to get rid of the "garbage" signatures
+	#signatures += ['Signature.20', 'Signature.26', 'Signature.29']
+	#signatures.remove('Signature.10')
+	#signatures.remove('Signature.11')
+	#signatures.remove('Signature.8')
+	
 	d = {}
 	for signature in signatures:
 		spectrumValue = spectrumDict[signature][quadNuc]
