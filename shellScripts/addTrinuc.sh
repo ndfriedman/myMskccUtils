464d7d03393086dfb324ea963b30110596253f4f
NoahFriedman <friedman@mskcc.org>
2019-10-04 12:29:46 -0400

    changes

--- shellScripts/addTrinuc.sh
+++ shellScripts/addTrinuc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python /ifs/work/taylorlab/friedman/myUtils/mutationSigUtils.py --mode trinucOnly --inputMaf /ifs/work/taylorlab/friedman/myAdjustedDataFiles/maf2mafAnnotatedMay16.maf --outputDir /ifs/work/taylorlab/friedman/myAdjustedDataFiles --outputFilename mafAnnoPlusTrinuc
+python /ifs/work/taylorlab/friedman/myUtils/mutationSigUtils.py --mode trinucOnly --inputMaf /ifs/work/taylorlab/friedman/myAdjustedDataFiles/impactMafs/data_mutations_unfiltered_reviewed_oncokb.txt --outputDir /ifs/work/taylorlab/friedman/myAdjustedDataFiles --outputFilename annotatedOncoPlusHotspotMafAllImpact_trinuc
